8652b5d1ea376d4284d5c462c0ac0241d01fa0a0
HoHoHoang <hoangdinh90@gmail.com>
2020-01-20 17:35:30 +0000

    added a path for our URL output

--- express_server.js
+++ express_server.js
@@ -30,6 +30,16 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
 
+ app.get("/urls", (req, res) => {
+   let templateVars = { urls: urlDatabase };
+   res.render("urls_index", templateVars);
+ });
+
+ app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
