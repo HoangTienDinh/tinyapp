d5455167e201935e2f5bfe0192029cd0560f4602
HoHoHoang <hoangdinh90@gmail.com>
2020-01-21 00:48:59 +0000

    added a new page to create a random string for user inputs on websites

--- express_server.js
+++ express_server.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require('body-parser');
+const randomstring = require('randomstring');
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
 });
 
+function generateRandomString() {
+  return randomstring.generate(6);
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send(generateRandomString());         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
